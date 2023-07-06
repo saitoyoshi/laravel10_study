df71f2250054a4745b8510bfae32da6cd61b3d0f
saitoyoshi <mitti180@gmail.com>
2023-07-06 10:09:58 +0900

    ヴァリデーションのためのRequestオブジェクトを追加

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BookPostRequest;
 use App\Models\Book;
 use App\Models\Category;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
@@ -31,7 +31,7 @@ class BookController extends Controller
             'categories' => $categories
         ]);
     }
-    public function store(Request $request): Book
+    public function store(BookPostRequest $request): Book
     {
         $book = new Book();
 
