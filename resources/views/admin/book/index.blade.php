b802385a5ccc3d4b78310f5a593452678b033237
saitoyoshi <mitti180@gmail.com>
2023-07-06 11:46:41 +0900

    匿名コンポーネントの練習

--- resources/views/admin/book/index.blade.php
+++ resources/views/admin/book/index.blade.php
@@ -13,22 +13,7 @@
     <div style="color:green">{{ session('message') }}</div>
     @endif
     <a href="{{ route('book.create') }}">追加</a>
-    <table border="1">
-        <tr>
-            <th>カテゴリ</th>
-            <th>タイトル</th>
-            <th>価格</th>
-        </tr>
-    @foreach ($books as $book)
-        <tr @if($loop->even) style="background-color:#eee;"
-
-        @endif>
-            <td>{{ $book->category->title }}</td>
-            <td>{{ $book->title }}</td>
-            <td>{{ $book->price }}</td>
-        </tr>
-    @endforeach
-</table>
+  <x-book-table :$books />
 </main>
 </body>
 </html>
---
+++ resources/views/components/book-table.blade.php
@@ -0,0 +1,16 @@
+<table border="1">
+    <tr>
+        <th>カテゴリ</th>
+        <th>タイトル</th>
+        <th>価格</th>
+    </tr>
+@foreach ($books as $book)
+    <tr @if($loop->even) style="background-color:#eee;"
+
+    @endif>
+        <td>{{ $book->category->title }}</td>
+        <td>{{ $book->title }}</td>
+        <td>{{ $book->price }}</td>
+    </tr>
+@endforeach
+</table>
