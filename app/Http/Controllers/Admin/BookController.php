b2c9e3d8cc85d7f805181d05d011275219f9c079
saitoyoshi <mitti180@gmail.com>
2023-07-06 11:19:34 +0900

    indexメソッドの返り値の型をResponseに変更

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -9,17 +9,21 @@ use App\Models\Category;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
 
 class BookController extends Controller
 {
-    public function index(): View
+    public function index(): Response
     {
         $books = Book::with('category')
         ->orderBy('category_id')
         ->orderBy('title')
         ->get();
 
-        return view('admin.book.index', ['books' => $books]);
+        return response()
+        ->view('admin.book.index', ['books' => $books])
+        ->header('Content-Type', 'text/html')
+        ->header('Content-Encoding', 'UTF-8');
     }
     public function show(string $id): Book
     {
