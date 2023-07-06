71e56cabe00f38b1f0ebdab3fbc6085139a18d2d
saitoyoshi <mitti180@gmail.com>
2023-07-06 12:00:46 +0900

    layoutコンポーネントの練習

--- resources/views/admin/book/index.blade.php
+++ resources/views/admin/book/index.blade.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>一覧ページ</title>
-</head>
-<body>
-    <main>
-    <h1>書籍一覧</h1>
+<x-layouts.book-manager>
+    <x-slot:title>
+        書籍一覧
+    </x-slot:title>
+<h1>書籍一覧</h1>
     @if(session('message'))
     <div style="color:green">{{ session('message') }}</div>
     @endif
     <a href="{{ route('book.create') }}">追加</a>
   <x-book-table :$books />
-</main>
-</body>
-</html>
+</x-layouts.book-manager>
