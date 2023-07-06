e71d9e4b6720d8e4e4a990a8eed1e92752522d9f
saitoyoshi <mitti180@gmail.com>
2023-07-06 10:18:22 +0900

    ヴァリデーションメッセージ日本語化

---
+++ lang/ja/validation.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    'exists' => '正しい :attribute を選択してください',
+    'max' => [
+        'numeric' => ':attribute は :max 以下を入力してください',
+        'string' => ':attribute は :max 文字以内で入力してください'
+    ],
+    'min' => [
+        'numeric' => ':attribute は :min 以上で入力してください',
+        'string' => ':attribute は :min 文字以上を入力してください'
+    ],
+    'numeric' => ':attribute は数値で入力してください',
+    'required' => ':attribute は必須入力です',
+    'unique' => ':attribute はすでに登録されています',
+
+    'attributes' => [
+        'category_id' => 'カテゴリ',
+        'title' => 'タイトル',
+        'price' => '価格',
+    ],
+];
