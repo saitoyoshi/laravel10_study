ee2f58930b5209718723905592a3ad8c04cd0d92
saitoyoshi <mitti180@gmail.com>
2023-07-14 11:19:36 +0900

    更新・登録フォーム共通化

--- resources/views/admin/book/create.blade.php
+++ resources/views/admin/book/create.blade.php
@@ -8,44 +8,7 @@
         </x-alert>
         <form action={{ route('book.store') }} method="POST">
             @csrf
-            <div>
-                <label for="">カテゴリ</label>
-                <select name="category_id" id="">
-                    @foreach ($categories as $category)
-                        <option value="{{ $category->id }}"
-                            @selected($category->id === old('category_id'))>
-                            {{ $category->title }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-            <div>
-                <label for="">タイトル</label>
-                <input type="text" name="title" value="{{ old('title') }}">
-            </div>
-            <div>
-                <label for="">価格</label>
-                <input type="text" name="price" value="{{ old('price') }}">
-            </div>
-            <div>
-                <label for="">著者</label>
-                <ul>
-                    @foreach ($authors as $author)
-                <li>
-                    <input type="checkbox" name="author_ids[]" id="{{ $author->id }}" value="{{ $author->id }}"
-                    @checked(
-                        is_array(old('author_ids'))
-                        &&
-                        in_array(
-                            $author->id,
-                            old('author_ids')
-                        )
-                    )>
-                    <label for="{{ $author->id }}">{{ $author->name }}</label>
-                </li>
-                @endforeach
-            </ul>
-            </div>
+            <x-book-form :$categories :$authors />
             <input type="submit" value="送信">
         </form>
     </x-layouts.book-manager>
--- resources/views/admin/book/edit.blade.php
+++ resources/views/admin/book/edit.blade.php
@@ -12,43 +12,7 @@
     <form action="{{ route('book.update', $book) }}" method="POST">
         @csrf
         @method('PUT')
-        <div>
-            <label for="">カテゴリ</label>
-            <select name="category_id" id="">
-                @foreach ($categories as $category)
-                    <option value="{{ $category->id }}" @selected(
-                        $category->id == old('category_id', $book->category_id)
-                    )>
-                    {{ $category->title }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
-        <div>
-            <label for="">タイトル</label>
-            <input type="text" name="title" value="{{ old('title', $book->title) }}">
-        </div>
-        <div>
-            <label for="">価格</label>
-            <input type="text" name="price" value="{{ old('price', $book->price) }}">
-        </div>
-        <div>
-            <label for="">著者</label>
-            <ul>
-                @foreach ($authors as $author)
-
-                <li>
-                    <input type="checkbox" name="author_ids[]" id="" value="{{ $author->id }}" @checked(
-                        in_array(
-                            $author->id,
-                            old('author_ids', $authorIds)
-                            )
-                            )>
-                    {{ $author->name }}
-                </li>
-                @endforeach
-            </ul>
-        </div>
+        <x-book-form :$categories :$authors  :$book :$authorIds  />
         <input type="submit" value="送信">
     </form>
 </x-layouts.book-manager>
