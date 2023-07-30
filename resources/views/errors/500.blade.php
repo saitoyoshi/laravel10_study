7218ec06840e3f8ceac28509f32cb71c88dd5f76
saitoyoshi <mitti180@gmail.com>
2023-07-30 15:13:08 +0900

    httpリクエストエラーのハンドリングの演習

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>500 Application Error</title>
+</head>
+<body>
+    <h1>500 Application Error</h1>
+    <p>アプリケーションエラーが発生しました。時間をおいて再度お試しください</p>
+</body>
+</html>
