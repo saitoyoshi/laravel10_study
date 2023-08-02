b1fea6d9ad77a9d27456f6dbefcf6f2f60b0a2be
saitoyoshi <mitti180@gmail.com>
2023-08-02 17:08:02 +0900

    リソースを使用し、欲しいJSONを作成する

--- app/Http/Controllers/API/MessageController.php
+++ app/Http/Controllers/API/MessageController.php
@@ -3,20 +3,22 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\MessageResource;
 use App\Models\Message;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Http\Response;
 
 class MessageController extends Controller
 {
-    public function index(): Collection {
+    public function index(): AnonymousResourceCollection {
         $messages = Message::all();
 
-        return $messages;
+        return MessageResource::collection($messages);
     }
-    public function show(Message $message): Message {
-        return $message;
+    public function show(Message $message): MessageResource {
+        return new MessageResource($message);
     }
     public function store(Request $request): Message {
         $message = new Message();
