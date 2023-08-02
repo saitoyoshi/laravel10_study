b1ea45b7b58af77e3829e887909b118e69b2b28f
saitoyoshi <mitti180@gmail.com>
2023-08-02 16:04:36 +0900

    更新処理のヴァリデーションテスト

--- tests/Feature/Admin/BookUpdate.php
+++ tests/Feature/Admin/BookUpdate.php
@@ -70,4 +70,41 @@ class BookUpdate extends TestCase
 
         $this->assertSame('Laravel book', $this->book->refresh()->title);
     }
+    /** @test */
+    public function 更新処理のヴァリデーション(): void {
+        $this->actingAs($this->admin, 'admin');
+        $url = route('book.update', $this->book);
+
+        $this->from(route('book.edit', $this->book))->put($url, ['category_id' => ''])->assertRedirect(route('book.edit', $this->book));
+
+        $this->put($url, ['category_id' => ''])->assertInvalid(['category_id' => 'カテゴリ は必須']);
+
+        $this->put($url, ['category_id' => '0'])->assertInvalid(['category_id' => '正しい カテゴリ']);
+
+        $this->put($url, ['category_id' => $this->categories[2]->id])->assertValid('category_id');
+
+        $this->put($url, ['title' => ''])->assertInvalid(['title' => 'タイトル は必須入力']);
+        $this->put($url, ['title' => 'a'])->assertValid('title');
+
+        $this->put($url, ['title' => str_repeat('a', 100)])->assertValid('title')->assertValid('title');
+
+        $this->put($url, ['title' => str_repeat('a', 101)])->assertInvalid(['title' => 'タイトル は 100 文字以内']);
+
+        $this->put($url, ['price' => 'a'])->assertInvalid(['price' => '価格 は数値']);
+        $this->put($url, ['price' => '0'])->assertInvalid(['price' => '価格 は 1 以上']);
+        $this->put($url, ['price' => '1'])->assertValid('price');
+        $this->put($url, ['price' => '9999'])->assertValid('price');
+        $this->put($url, ['price' => '10000'])->assertInvalid(['price' => '価格 は 9999 以下']);
+
+        $this->put($url, ['author_ids' => []])->assertInvalid(['author_ids' => '著者 は必須入力']);
+
+        $this->put($url, ['author_ids' => ['0']])->assertInvalid(['author_ids.0' => '正しい 著者 を選択']);
+
+        $this->put($url, ['author_ids' => [$this->authors[2]->id]])->assertValid('author_ids.0');
+
+
+
+
+
+    }
 }
