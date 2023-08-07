52d5f93125712785feeda94f6281f2bc135f98d0
saitoyoshi <mitti180@gmail.com>
2023-08-07 11:10:58 +0900

    ルーティングをグループ化

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,8 @@ Route::prefix('/messages')->controller(MessageController::class)->name('messages
     Route::delete('/{message}', 'delete')->name('destroy');
 });
 
-Route::get('admin/books', [BookController::class, 'index'])->name('book.index');
-Route::get('admin/books/{id}', [BookController::class, 'show'])->whereNumber('id')->name('book.show');
+Route::prefix('admin/books')->controller(BookController::class)->name('book')->group(function () {
+    Route::get('', 'index')->name('index');
+    Route::get('/{id}', 'show')->whereNumber('id')->name('show');
+
+});
