8ba251c53dd08758e522c6b70abc90d96cb52989
saitoyoshi <mitti180@gmail.com>
2023-08-07 10:22:53 +0900

    各テーブルの未グレーションファイルとシーダーファイル、factoryの練習

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(MessageTableSeeder::class);
+        $this->call([
+            AuthorsTableSeeder::class,
+            BooksTableSeeder::class,
+            AuthorBookTableSeeder::class,
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
