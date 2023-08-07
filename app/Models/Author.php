8ac90741cdd529aa316c583568a524511a7c3b6d
saitoyoshi <mitti180@gmail.com>
2023-08-07 10:57:11 +0900

    リレーション設定

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -5,11 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Author extends Model
 {
     use HasFactory;
     public function books(): BelongsToMany {
         return $this->belongsToMany(Book::class)->withTimestamps();
+    }
+    public function detail(): HasOne {
+        return $this->hasOne(AuthorDetail::class);
     }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Book extends Model
@@ -11,5 +12,8 @@ class Book extends Model
     use HasFactory;
     public function authors(): BelongsToMany {
         return $this->belongsToMany(Author::class)->withTimestamps();
+    }
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
     }
 }
