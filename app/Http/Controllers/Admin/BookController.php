19676b353af20d3348f3c76823e2b8561c3d628b
saitoyoshi <mitti180@gmail.com>
2023-08-07 11:26:01 +0900

    レクエストオブジェクトから値を受け取る練習

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class BookController extends Controller
 {
@@ -15,6 +17,19 @@ class BookController extends Controller
     }
     public function show(string $id): Book {
         $book = Book::findOrFail($id);
+        return $book;
+    }
+    public function create(): View {
+        $categories = Category::all();
+        return view('admin.book.create', compact('categories'));
+    }
+    public function store(Request $request): Book {
+        $book = new Book();
+        $book->category_id = $request->category_id;
+        $book->title = $request->title;
+        $book->price = $request->price;
+
+        $book->save();
         return $book;
     }
 }
---
+++ resources/views/admin/book/create.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <h1>書籍登録</h1>
+        <form action="{{ route('book.store') }}" method="post">
+            @csrf
+            <div>
+                <label for="">カテゴリ</label>
+                <select name="category_id" id="">
+                    @foreach ($categories as $category)
+                    <option value="{{ $category->id }}">
+                        {{ $category->title }}
+                    </option>
+
+                    @endforeach
+                </select>
+            </div>
+            <div>
+                <label for="">タイトル</label>
+                <input type="text" name="title">
+            </div>
+            <div>
+                <label for="">価格</label>
+                <input type="text" name="price">
+            </div>
+            <button>送信</button>
+        </form>
+    </main>
+</body>
+</html>
