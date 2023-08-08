f0a4dab85ca648a0617cb96889d287251ed472eb
saitoyoshi <mitti180@gmail.com>
2023-08-08 15:16:04 +0900

    書籍のヴァリデーションメッセージの日本語化

--- lang/ja/validation.php
+++ lang/ja/validation.php
@@ -1,12 +1,24 @@
 <?php
 
 return [
+    'exists' => '正しい :attribute を選択してください',
     'required' => ':attribute を入力してください',
-    'attributes' => [
-        'message' => 'メッセージ',
-        'body' =>'メッセージ',
+    'numeric' => ':attribute は数値で入力してください',
+    'unique' => ':attribute はすでに登録されています',
+
+    'min' => [
+        'numeric' => ':attribute は :min 以上を入力してください',
+        'string' => ':attribute は :min 文字以上を入力してください',
     ],
     'max' => [
         'string' => ':attribute は :max 文字以下で入力してください',
-    ]
+        'numeric' => ':attribute は :max 以下で入力してください',
+    ],
+    'attributes' => [
+        'category_id' => 'カテゴリ',
+        'title' => 'タイトル',
+        'price' => '価格',
+        'message' => 'メッセージ',
+        'body' =>'メッセージ',
+    ],
 ];
