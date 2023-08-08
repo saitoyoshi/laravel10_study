22aaeb851e8022d289b9371a4e2b8dbfe4b114f5
saitoyoshi <mitti180@gmail.com>
2023-08-08 17:16:06 +0900

    書籍登録後一覧画面に遷移する

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -7,14 +7,15 @@ use App\Http\Requests\BookPostRequest;
 use App\Models\Book;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class BookController extends Controller
 {
-    public function index(): Collection {
-        $books = Book::all();
-        return $books;
+    public function index(): View {
+        $books = Book::with('category')->orderBy('category_id')->orderBy('title')->get();
+        return view('admin.book.index', compact('books'));
     }
     public function show(string $id): Book {
         $book = Book::findOrFail($id);
@@ -24,13 +25,13 @@ class BookController extends Controller
         $categories = Category::all();
         return view('admin.book.create', compact('categories'));
     }
-    public function store(BookPostRequest $request): Book {
+    public function store(BookPostRequest $request): RedirectResponse {
         $book = new Book();
         $book->category_id = $request->category_id;
         $book->title = $request->title;
         $book->price = $request->price;
 
         $book->save();
-        return $book;
+        return redirect(route('book.index'))->with('message', $book->title . 'を登録しました');
     }
 }
