e72ba258ce1415812ccab89119e3a3648a4e7cae
saitoyoshi <mitti180@gmail.com>
2023-08-10 10:37:08 +0900

    更新画面、登録画面のフォームを共通化

--- resources/views/admin/book/create.blade.php
+++ resources/views/admin/book/create.blade.php
@@ -11,38 +11,7 @@
 
         <form action="{{ route('book.store') }}" method="post">
             @csrf
-            <div>
-                <label for="">カテゴリ</label>
-                <select name="category_id" id="">
-                    @foreach ($categories as $category)
-                    <option value="{{ $category->id }}" @selected($category->id == old('category_id'))>
-                        {{ $category->title }}
-                    </option>
-                    @endforeach
-                </select>
-            </div>
-            <div>
-                <label for="">タイトル</label>
-                <input type="text" name="title" value="{{ old('title') }}">
-            </div>
-            <div>
-                <label for="">価格</label>
-                <input type="text" name="price" value="{{  old('price') }}">
-            </div>
-            <div>
-                <label for="">著者</label>
-                <ul>
-                @foreach ($authors as $author)
-                    <li>
-                        <input type="checkbox" name="author_ids[]" value="{{ $author->id }}"
-                        @checked(is_array(old('author_ids'))
-                        &&
-                        in_array($author->id, old('author_ids')))>
-                        {{ $author->name }}
-                    </li>
-                @endforeach
-            </ul>
-            </div>
+            <x-book-form :$categories :$authors></x-book-form>
             <button>送信</button>
         </form>
     </x-layouts>
